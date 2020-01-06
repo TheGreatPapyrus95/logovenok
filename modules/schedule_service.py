8723264a22116110b51791c472c61c25ace870ce
TheGreatPapyrus95 <a.kungurowa@yandex.ru>
2020-01-06 14:17:40 +0300

    Rename EventService

--- modules/schedule_service.py
+++ modules/schedule_service.py
@@ -7,34 +7,33 @@ import traceback
 import vk
 
 
-class EventService(threading.Thread):
+class ScheduleService(threading.Thread):
 	daemon = True
 
 	def __init__(self):
 		super().__init__()
 		self.stop = False
-		self._events = self._load_events()
+		self._schedule = self._load_schedule()
 
 	def run(self):
-		print('start event service')
+		print('start schedule service')
 		while True:
 			time.sleep(10)
 			if self.stop:
 				break
 
-			for event in self._events:
+			for event in self._schedule:
 				trigger_time = time.mktime(datetime.datetime(*event.time).timetuple()) - time.timezone
 				delta = abs(time.time() - trigger_time)
-				if delta <= 10:
+				if delta < 5:
 					for chat_id, peer_id in vk.get_chats():
 						try:
 							file = random.choice(event.attachment)
 							vk.send_message(chat_id, event.message, [file], peer_id)
-							time.sleep(1)
 						except:
 							print(traceback.format_exc())
 
-	def _load_events(self):
+	def _load_schedule(self):
 		with open('./data/events.json', encoding='utf-8') as f:
 			return [
 				Event(x) for x in json.loads(f.read())
